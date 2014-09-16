9e4cf05db3867f0c52b45a94e538d79b7212cbc0
Herr von Boedefeld <herrvonbdf@gmail.com>
2014-09-16 09:40:00 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  var myBuffer = fs.readFileSync(index.html)
+  var myBuffer = fs.readFileSync('index.html')
   var myString = buf.toString(myBuffer)
   response.send(myString)
 })
