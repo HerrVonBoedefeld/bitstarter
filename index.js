d98637e8cec21a364729154fb7ae08279628c3b4
Herr von Boedefeld <herrvonbdf@gmail.com>
2014-09-16 09:51:03 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var buf = require('buffer');
 var express = require('express')
 var app = express();
 
@@ -8,7 +7,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   var myBuffer = fs.readFileSync('index.html')
-  var myString = buf.toString(myBuffer)
+  var myString = myBuffer.toString()
   response.send(myString)
 })
 
